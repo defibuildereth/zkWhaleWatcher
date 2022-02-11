38cbf321d6b65ebf82876b82840ffa40b3f9cbe8
d7o1d1s0 <d7sarmy@gmail.com>
2022-02-10 21:13:05 -0500

    basically works :)

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const pollApi = async function () {
                 // console.log(data)
                 let numNewBlocks = data.result.list.length;
                 if (numNewBlocks > 1) {
-                    console.log('there are ', numNewBlocks-1, ' new blocks to parse')
+                    console.log('there are ', numNewBlocks - 1, ' new blocks to parse')
                     for (let i = 1; i < numNewBlocks; i++) {
                         blocksToParse.push(data.result.list[i].blockNumber)
                     }
@@ -33,33 +33,78 @@ const pollApi = async function () {
 const parseBlocksArray = async function (array) {
     for (let i = 0; i < array.length; i++) {
         await parseBlock(array[i], 'latest', 0)
-            // .then(res => res.json())
-            
+        // .then(res => console.log(res))
+
     }
 }
 
 const parseBlock = async function (block, tx, index) {
     let blockTxArray = []
-    console.log('calling parseBlock with block: ', block, ' tx: ', tx)
+    // console.log('calling parseBlock with block: ', block, ' tx: ', tx)
     await fetch(`https://api.zksync.io/api/v0.2/blocks/${block}/transactions?from=${tx}&limit=100&direction=older`)
         .then((res) => res.json())
         .then(data => {
             for (let i = index; i < data.result.list.length; i++) {
                 if (data.result.list[i].op.type == "Swap") {
-                blockTxArray.push(data.result.list[i])
+                    blockTxArray.push(data.result.list[i])
                 }
             }
 
             if (data.result.list.length > 99) {
                 parseBlock(block, data.result.list[99].txHash, 1)
             } else {
-                console.log('swap txs in this block: ', blockTxArray.length)
+                getTotalBlockVolume(block, blockTxArray)
             }
         })
 }
 
-const parseData = async function (data) {
-    let blockNumber = data.blockNumber
+const getTotalBlockVolume = async function (block, array) {
+
+    let volPromiseArray = []
+
+    for (let i = 0; i < array.length; i++) {
+        volPromiseArray.push(getTransactionVolume(array[i]))
+    }
+
+    await Promise.all(volPromiseArray)
+        .then((values) => {
+            let totalBlockVolume = 0;
+
+            // console.log('values.length: ', values.length)
+            for (let i = 0; i < values.length; i++) {
+                if (values[i]) {
+                    totalBlockVolume = totalBlockVolume + values[i]
+
+                }
+            }
+            // console.log('block: ', block, 'volume: ', totalBlockVolume);
+            parseData(block, totalBlockVolume, array.length)
+
+        })
+}
+
+const getTransactionVolume = async function (tx) {
+    let txVol = 0;
+    if (tx.op.type == 'Swap') {
+        // console.log(tx)
+        // console.log('amount: ',tx.op.orders[1].amount)
+        let token = tx.op.orders[1].tokenSell
+        await fetch(`https://api.zksync.io/api/v0.2/tokens/${token}/priceIn/usd`)
+            .then((res) => res.json())
+            .then(data => {
+                // console.log(data)
+                let decimal = data.result.decimals;
+                let amount = tx.op.orders[1].amount;
+                let price = data.result.price;
+                txVol = amount * 10 ** (0 - decimal) * price
+                // console.log(roughVol)
+            })
+        return txVol
+    }
+}
+
+const parseData = async function (block, totalBlockVolume, numTxs) {
+    let blockNumber = block
     let payload = {
         "username": "ZZ Whale Watcher",
         "content": "",
@@ -71,36 +116,23 @@ const parseData = async function (data) {
                 },
                 "title": `New Block ${blockNumber}`,
                 "url": `https://api.zksync.io/api/v0.2/blocks/${blockNumber}`,
-                "description": "**New** __block__ `mined`[hyperlink](https://trade.zigzag.exchange/)",
+                
                 "color": 15258703,
                 "fields": [
                     {
-                        "name": "Can",
-                        "value": "Make",
+                        "name": "**Swaps**",
+                        "value": `${numTxs}`,
                         "inline": true
                     },
                     {
-                        "name": "These",
-                        "value": "Sick",
+                        "name": "**Total Volume**",
+                        "value": `${totalBlockVolume.toFixed(2)}`,
                         "inline": true
-                    },
-                    {
-                        "name": "Use `\"inline\": true` parameter, if you want to display fields in the same line.",
-                        "value": "okay..."
-                    },
-                    {
-                        "name": "Thanks!",
-                        "value": "You're welcome :wink:"
                     }
+                    
                 ],
-                "thumbnail": {
-                    "url": "https://upload.wikimedia.org/wikipedia/commons/3/38/4-Nature-Wallpapers-2014-1_ukaavUI.jpg"
-                },
-                "image": {
-                    "url": "https://upload.wikimedia.org/wikipedia/commons/5/5a/A_picture_from_China_every_day_108.jpg"
-                },
                 "footer": {
-                    "text": "Woah! So cool! :smirk:",
+                    "text": "Made by DefiBuilder.eth, powered by ZkSync API",
                     "icon_url": "https://i.imgur.com/fKL31aD.jpg"
                 }
             }
@@ -118,13 +150,15 @@ const pingWebhook = async function (payload) {
             // console.log(res)
         })
         .catch(error => {
-            console.error(error)
+            console.error(error.response.statusText)
         })
 }
 
-// pollApi()
+pollApi()
+
+// getTotalBlockVolume(mostRecentBlock)
 
 // parseData(mostRecentBlock)
 
-// parseBlocksArray([mostRecentBlock, mostRecentBlock+1])
+// parseBlocksArray([mostRecentBlock+3])
 
